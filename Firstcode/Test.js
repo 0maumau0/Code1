4e8240d441449823d2715227a7afae51b3ad60ba
0maumau0 <strubemaurice@gmail.com>
2025-06-25 14:03:38 +0200

    js and ts cahnged a bit for task 04.4

--- Firstcode/Test.js
+++ Firstcode/Test.js
@@ -1,2 +1,29 @@
 "use strict";
-console.log("Hello World");
+console.log("I like Overlord");
+let v = 1;
+v = v + 1;
+console.log(v);
+let a = ["hi", 7, "bye"];
+let greek;
+let speek;
+let right;
+speek = "Hallo";
+greek = 8;
+right = true;
+greek = greek + greek;
+console.log(greek);
+console.log(0.1 + 0.2); // flawed calculation
+// area calculation Triangle
+let w = 5;
+let h = 3;
+let c;
+c = 2;
+w = w / c;
+c = w * h;
+console.log(c);
+//rectangle w* h =a
+//oh noo task
+let t = "Oh no! ";
+t = t + t;
+t = t + t;
+//swap made a new variable and save x or y short in it
--- Firstcode/Test.ts
+++ Firstcode/Test.ts
@@ -1,1 +1,30 @@
-console.log("Hello World")
+console.log("I like Overlord");
+let v: number= 1;
+v=v+1;
+console.log(v);
+let a = ["hi", 7, "bye"]
+let greek: number;
+let speek: string;
+let right: boolean;
+speek = "Hallo";
+greek = 8;
+right = true;
+greek = greek + greek;
+console.log(greek)
+console.log(0.1 + 0.2); // flawed calculation
+
+// area calculation Triangle
+let w: number = 5;
+let h: number = 3;
+let c: number;
+c = 2;
+w = w / c;
+c = w * h;
+console.log(c)
+//rectangle w* h =a
+
+//oh noo task
+let t: string = "Oh no! ";
+t = t + t;
+t = t + t;
+//swap made a new variable and save x or y short in it
