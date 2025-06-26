fa74181acd174e27c5c383e50d6ecc6c3859e3bb
0maumau0 <strubemaurice@gmail.com>
2025-06-26 10:37:09 +0200

    index to Test and blocks, coding changes, testjs map

--- Firstcode/Test.js
+++ Firstcode/Test.js
@@ -1,19 +1,22 @@
 "use strict";
-console.log("I like Overlord");
-let v = 1;
-v = v + 1;
-console.log(v);
-let a = ["hi", 7, "bye"];
-let greek;
-let speek;
-let right;
-speek = "Hallo";
-greek = 8;
-right = true;
-greek = greek + greek;
-console.log(greek);
-console.log(0.1 + 0.2); // flawed calculation
-// area calculation Triangle
+var Test;
+(function (Test) {
+    console.log("I like Overlord");
+    let v = 1;
+    v = v + 1;
+    console.log(v);
+    let a = ["hi", 7, "bye"];
+    let greek;
+    let speek;
+    let right;
+    speek = "Hallo";
+    greek = 8;
+    right = true;
+    greek = greek + greek;
+    console.log(greek);
+    console.log(0.1 + 0.2); // flawed calculation
+})(Test || (Test = {}));
+// area calculation Triangle Rectangle w*h
 let w = 5;
 let h = 3;
 let c;
@@ -21,9 +24,28 @@ c = 2;
 w = w / c;
 c = w * h;
 console.log(c);
-//rectangle w* h =a
 //oh noo task
 let t = "Oh no! ";
 t = t + t;
 t = t + t;
-//swap made a new variable and save x or y short in it
+console.log(t);
+//nim try 
+let z0 = 4;
+let z1 = 7;
+let z2 = 5;
+let z3 = 3;
+let result;
+result = z0 ^ z1;
+result = result ^ z2;
+result = z1 ^ z2;
+result = result ^ z3;
+console.log(result);
+{
+    let x = 0;
+    {
+        //let x: number = 1;
+        console.log(x);
+    }
+    console.log(x);
+}
+//# sourceMappingURL=Test.js.map
--- Firstcode/Test.ts
+++ Firstcode/Test.ts
@@ -1,19 +1,22 @@
-console.log("I like Overlord");
-let v: number= 1;
-v=v+1;
-console.log(v);
-let a = ["hi", 7, "bye"]
-let greek: number;
-let speek: string;
-let right: boolean;
-speek = "Hallo";
-greek = 8;
-right = true;
-greek = greek + greek;
-console.log(greek)
-console.log(0.1 + 0.2); // flawed calculation
+namespace Test{
 
-// area calculation Triangle
+    console.log("I like Overlord");
+    let v: number= 1;
+    v=v+1;
+    console.log(v);
+    let a = ["hi", 7, "bye"]
+    let greek: number;
+    let speek: string;
+    let right: boolean;
+    speek = "Hallo";
+    greek = 8;
+    right = true;
+    greek = greek + greek;
+    console.log(greek)
+    console.log(0.1 + 0.2); // flawed calculation
+}
+    
+// area calculation Triangle Rectangle w*h
 let w: number = 5;
 let h: number = 3;
 let c: number;
@@ -21,13 +24,12 @@ c = 2;
 w = w / c;
 c = w * h;
 console.log(c)
-//rectangle w* h =a
 
 //oh noo task
 let t: string = "Oh no! ";
 t = t + t;
 t = t + t;
-//swap made a new variable and save x or y short in it
+console.log(t)
 
 //nim try 
 let z0: number = 4;
@@ -39,4 +41,13 @@ result = z0 ^ z1;
 result = result ^ z2;
 result = z1 ^ z2;
 result = result ^ z3;
-console.log(result)
+console.log(result);
+
+{
+    let x:number =0;
+    {
+    //let x: number = 1;
+    console.log(x);
+    }
+    console.log(x);
+}
