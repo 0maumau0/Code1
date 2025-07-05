69d99c00f96b2924d2e3d7a4c9c7fe2155aa7818
0maumau0 <strubemaurice@gmail.com>
2025-07-05 12:57:02 +0200

    try the inspektor task idont know how it works changes by event

--- Firstcode/Events/Event.js
+++ Firstcode/Events/Event.js
@@ -1,4 +1,22 @@
 "use strict";
-//namespace Events {
-//  console.log("Hello World");
+var eventInspektor;
+(function (eventInspektor) {
+    window.addEventListener("load", handleLoad);
+    function handleLoad(_event) {
+        eventListener();
+        moveonMouse;
+    }
+    function eventListener() {
+        const divs = document.querySelectorAll('div');
+        document.addEventListener("mousemove", moveonMouse);
+        divs.forEach(div => {
+            div.addEventListener("click", mouseclick);
+            div.addEventListener("keyup", key);
+        });
+    }
+    function moveonMouse(_event) {
+        console.log(_event);
+        console.log(_event.target);
+    }
+})(eventInspektor || (eventInspektor = {}));
 //# sourceMappingURL=Event.js.map
--- Firstcode/Events/Event.ts
+++ Firstcode/Events/Event.ts
@@ -1,17 +1,37 @@
-//namespace Events {
-  //  console.log("Hello World");
-
-    //document.addEventListener("DOMContentLoaded", handleLoad);
-
-
-    //function handleLoad(_event: Event): void {
-      //  console.log(_event)
-    //}
-///}
-
 namespace eventInspektor{
 
+  window.addEventListener("load",handleLoad);
+  
+  function handleLoad(_event: Event):void{
+
+    eventListener();
+    moveonMouse
+    
 
 
+  }
+  
+  function eventListener():void{
+
+    const divs =document.querySelectorAll('div')
+     document.addEventListener("mousemove", moveonMouse );
+    divs.forEach(div => {
+      div.addEventListener("click", mouseclick);
+      div.addEventListener("keyup", key);
+    });
+
+  }
+
+  function moveonMouse(_event: MouseEvent){
+    console.log(_event)
+    console.log(_event.target)
+    
+    
+    
+
+
+  }
+  
+  
 
 }
