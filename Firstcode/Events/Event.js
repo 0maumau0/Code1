21b92e818edcc0b6ff1f9fb88b737604b1e20595
0maumau0 <strubemaurice@gmail.com>
2025-07-06 14:19:51 +0200

    tryed a bit on the inspector code

--- Firstcode/Events/Event.js
+++ Firstcode/Events/Event.js
@@ -4,19 +4,33 @@ var eventInspektor;
     window.addEventListener("load", handleLoad);
     function handleLoad(_event) {
         eventListener();
-        moveonMouse;
+        handleMove();
+        handleclick();
+        handlekeypress();
     }
     function eventListener() {
         const divs = document.querySelectorAll('div');
-        document.addEventListener("mousemove", moveonMouse);
+        document.addEventListener("mousemove", handleMove);
         divs.forEach(div => {
-            div.addEventListener("click", mouseclick);
-            div.addEventListener("keyup", key);
+            div.addEventListener("click", handleclick);
+            div.addEventListener("keyup", handlekeypress);
         });
     }
-    function moveonMouse(_event) {
+    // problem: span dont have settet size and inner text isnt completly
+    function handleMove(_event) {
         console.log(_event);
-        console.log(_event.target);
+        //console.log(_event.target)
+        document.getElementById("cursorSpan");
+        let span = document.getElementById("cursorSpan");
+        span.innerText = (_event.screenX, "x", _event.screenY, "y");
+        console.log(_event.screenX, "x", _event.screenY, "y", "oakdiiosajuifnjaouijfuiohuioashjf");
+    }
+    function handleclick(_event) {
+        if (_event.currentTarget == window) {
+            console.log("ICh mag fische");
+        }
+    }
+    function handlekeypress() {
     }
 })(eventInspektor || (eventInspektor = {}));
 //# sourceMappingURL=Event.js.map
--- Firstcode/Events/Event.ts
+++ Firstcode/Events/Event.ts
@@ -5,8 +5,9 @@ namespace eventInspektor{
   function handleLoad(_event: Event):void{
 
     eventListener();
-    moveonMouse
-    
+    handleMove();
+    handleclick();
+    handlekeypress();
 
 
   }
@@ -14,24 +15,39 @@ namespace eventInspektor{
   function eventListener():void{
 
     const divs =document.querySelectorAll('div')
-     document.addEventListener("mousemove", moveonMouse );
+     document.addEventListener("mousemove", handleMove );
     divs.forEach(div => {
-      div.addEventListener("click", mouseclick);
-      div.addEventListener("keyup", key);
+      div.addEventListener("click", handleclick);
+      div.addEventListener("keyup", handlekeypress);
+      
     });
 
   }
 
-  function moveonMouse(_event: MouseEvent){
+  // problem: span dont have settet size and inner text isnt completly
+  function handleMove(_event: MouseEvent):void{
     console.log(_event)
-    console.log(_event.target)
+    //console.log(_event.target)
+    document.getElementById("cursorSpan")
+    let span = document.getElementById("cursorSpan");
+    span!.innerText= (_event.screenX ,"x", _event.screenY, "y");
+    console.log(_event.screenX ,"x", _event.screenY, "y","oakdiiosajuifnjaouijfuiohuioashjf");
     
     
     
+
+  }
+
+  function handleclick(_event:Event):void{
+    if(_event.currentTarget == window){
+      console.log("ICh mag fische")
+    }
 
 
   }
   
-  
+  function handlekeypress(){
+
+  }
 
 }
