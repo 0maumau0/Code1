90b4cfbdea9932bfc7e08027cb9c124407827c30
0maumau0 <strubemaurice@gmail.com>
2025-07-08 16:45:32 +0200

    changes on animation much much code and setup for  tankrace

--- Firstcode/animation/animation.js
+++ Firstcode/animation/animation.js
@@ -1,32 +1,87 @@
 "use strict";
 var animation;
 (function (animation) {
+    //let blackHole = document.elementFromPoint(2, 2);
     window.addEventListener("load", handleLoad);
     const balls = [];
+    let timePreviousFrame = Date.now();
     function handleLoad() {
-        for (let i = 0; i <= 3; i++) {
-            const ball = {
-                element: document.createElement("span"),
-                position: { x: 100 + 20 * i, y: 100 + 20 * i },
-                velocity: { x: 10, y: 10 },
-            };
-            document.body.appendChild(ball.element);
-            //balls[i] = ball
-            balls.push(ball);
+        document.body.addEventListener("click", handlClick);
+        for (let i = 0; i < 2; i++) {
+            createball();
         }
-        move();
+        update();
     }
-    function move() {
+    function update() {
+        const timeCurrent = Date.now();
+        let timeDelta = timeCurrent - timePreviousFrame;
+        timeDelta /= 1000;
+        move(timeDelta);
+        checkCollisionAll(),
+            timePreviousFrame = timeCurrent;
+        setTimeout(update, 16);
+    }
+    function move(_timeDelta) {
         for (const ball of balls) {
             // for (let i = balls.length; i >= balls.length; i++) { 
-            ball.position.x += ball.velocity.x;
-            ball.position.y += ball.velocity.y;
+            ball.position.x += ball.velocity.x * _timeDelta;
+            ball.position.y += ball.velocity.y * _timeDelta;
             //let ball: Ball = balls[i];
             ball.position.x = (ball.position.x + window.innerWidth) % window.innerWidth;
             ball.position.y = (ball.position.y + window.innerHeight) % window.innerHeight;
             ball.element.style.transform = "matrix(20,0,0,20," + ball.position.x + "," + ball.position.y + ")";
         }
-        setTimeout(move, 16);
     }
+    function handlClick(_event) {
+        let target = (_event.target);
+        console.log(target.tagName);
+        if (target.tagName == "SPAN") {
+            let targetParent = target.parentElement;
+            targetParent?.removeChild(target);
+        }
+        if (target.tagName === "BODY") {
+            createball();
+        }
+        // zur aufgabe auf den punkt wo geklickt wurde müsste man die funktion variable machen in dem man 
+        //die parameter allgemeinhält und jeweils reingibt
+    }
+    function createball() {
+        const ball = {
+            element: document.createElement("span"),
+            position: { x: 100 + Math.floor(Math.random() * 300) + 2, y: 100 + (Math.random() * 300) + 2 },
+            velocity: { x: 25 + Math.floor(Math.random() * 50) - 50, y: 25 + Math.floor(Math.random() * 50) - 50 },
+        };
+        document.body.appendChild(ball.element);
+        //balls[i] = ball
+        balls.push(ball);
+    }
+    function checkCollisionAll() {
+        for (const a in balls) {
+            for (let b = Number(a) + 1; b < balls.length; b++) {
+                checkCollision(Number(a), b);
+                // let distance: number = Math.hypot(balls[a].position.x);
+                // let threshhold: number = 2;
+                // if (distance < threshhold){
+                //     //watch mina to solve
+                // }
+            }
+        }
+    }
+    function checkCollision(_a, _b) {
+        let deltax = balls[_a].position.x - balls[_b].position.x;
+        let deltay = balls[_a].position.y - balls[_b].position.y;
+        let collision;
+    }
+    // for (let i = balls.length; i >= 0; i--){
+    //war nen versuch
+    // console.log (balls[i]);
+    //   console.log (target.id);
+    //   let ballid = Number(target.id)
+    // if (balls[i].id == ballid ){
+    //  console.log()
+    // else{
+    // document.createElement(
+    //create objekt
+    // }
 })(animation || (animation = {}));
 //# sourceMappingURL=animation.js.map
